afb5fff546c95541176baa5743984e886be40871
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2025-11-05 08:45:08 -0500

    Small fixes

--- src/routes/returnOEmbed.ts
+++ src/routes/returnOEmbed.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
-import { config } from '../config';
 
 export function returnOEmbed(req: Request, res: Response) {
   return res.json({
     provider_name: `SkyEmbed - 💬 ${req.query.replies} 🔁 ${req.query.reposts} ❤️ ${req.query.likes}`,
-    provider_url: config.BASE_URL,
+    author_url: req.query.url,
+    provider_url: req.query.url,
     description: req.query.description,
     title: req.query.description,
     author_name: req.query.description
